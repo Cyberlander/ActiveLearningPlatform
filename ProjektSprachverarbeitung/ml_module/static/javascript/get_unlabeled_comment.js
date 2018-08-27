8de1ab5694fedba1b9210add2e0288c31af1b242
Cyberlander <felix.froehlich1@gmx.net>
2018-08-27 12:55:53 +0200

    js refactoring

---
+++ ProjektSprachverarbeitung/ml_module/static/javascript/get_unlabeled_comment.js
@@ -0,0 +1,85 @@
+
+$(document).ready(
+    function(){
+      $("#good_button").click(
+        function(){
+          showSaveLabel( true );
+          currentLabelNumerical = 2;
+          document.getElementById('comment_panel').className = 'panel';
+          document.getElementById('comment_panel').classList.add('panel-success');
+        }
+      );
+});
+$(document).ready(
+    function(){
+      $("#neutral_button").click(
+        function(){
+          showSaveLabel( true );
+          currentLabelNumerical = 1;
+          document.getElementById('comment_panel').className = 'panel';
+          document.getElementById('comment_panel').classList.add('panel-warning');
+        }
+      );
+  });
+$(document).ready(
+    function(){
+      $("#bad_button").click(
+        function(){
+          showSaveLabel( true );
+          currentLabelNumerical = 0;
+          document.getElementById('comment_panel').className = 'panel';
+          document.getElementById('comment_panel').classList.add('panel-danger');
+        }
+      );
+  });
+$(document).ready(
+    function(){
+      $("#comment_button").click(
+        function(){
+          showSaveLabel( false );
+          document.getElementById('comment_panel').className = 'panel';
+          document.getElementById('comment_panel').classList.add('panel-primary');
+          $.ajax( { type:"GET",
+                    url:"http://localhost:8000/get_unlabeled_comment",
+                    //url:"{% static 'ajax_info.txt' %}",
+                    //dataType:"text",
+                    dataType:"json",
+                    error: function() {
+                      console.log("error");
+                    },
+                    beforeSend: function(){
+                      // Show image container
+                      $("#panel_content").html("Loading");
+                    },
+                    success: function(result){
+                    console.log(result)
+                    console.log( typeof result )
+                    console.log( result.Message )
+                    console.log( myGlobalVar )
+                    $("#panel_content").html(result.Message);
+                    currentId = result.Id
+                    currentComment = result.Message;
+                    currentLabelMachine = label_dict[result.Predicted]
+                    computer_says = "Computer says <strong>" + result.Predicted + "</strong>"
+                    $("#classification_guess").html( computer_says )
+
+                    $("#neural_network_prediction").html( result.Predicted )
+
+                    var alert_class = "alert-info"
+                    //change the color of the alert
+                    if ( result.Predicted == "negative" ){
+                      alert_class = "alert-danger"
+                    }
+                    else if ( result.Predicted == "neutral" ){
+                      alert_class = "alert-warning"
+                    }
+                    else if ( result.Predicted == "positive" ){
+                      alert_class = "alert-success"
+                    }
+                    document.getElementById('classification_guess').className = 'alert';
+                    document.getElementById('classification_guess').classList.add( alert_class );
+              }
+            });
+        }
+      );
+    });
---
+++ ProjektSprachverarbeitung/ml_module/static/javascript/save_labeled_comment.js
@@ -0,0 +1,61 @@
+$(document).ready(
+  function(){
+$("#label_button").click(
+        function(){
+
+
+          $.post("http://localhost:8000/send_comment_label",
+          {
+            id: currentId,
+            comment: currentComment,
+            label_user: currentLabelNumerical
+          },
+          function(data, status){
+            alert(label_user)
+          });
+
+
+          showSaveLabel( false );
+          document.getElementById('comment_panel').className = 'panel';
+          document.getElementById('comment_panel').classList.add('panel-primary');
+          $.ajax( { type:"GET",
+                    url:"http://localhost:8000/get_unlabeled_comment",
+                    //url:"{% static 'ajax_info.txt' %}",
+                    //dataType:"text",
+                    dataType:"json",
+                    error: function() {
+                      console.log("error");
+                    },
+                    beforeSend: function(){
+                      // Show image container
+                      $("#panel_content").html("Loading");
+                    },
+                    success: function(result){
+                      $("#panel_content").html(result.Message);
+                      currentId = result.Id
+                      currentComment = result.Message;
+                      currentLabelMachine = label_dict[result.Predicted];
+                      computer_says = "Computer says <strong>" + result.Predicted + "</strong>"
+                      $("#classification_guess").html( computer_says )
+
+
+                      $("#neural_network_prediction").html( result.Predicted )
+
+                      var alert_class = "alert-info"
+                      //change the color of the alert
+                      if ( result.Predicted == "negative" ){
+                        alert_class = "alert-danger"
+                      }
+                      else if ( result.Predicted == "neutral" ){
+                        alert_class = "alert-warning"
+                      }
+                      else if ( result.Predicted == "positive" ){
+                        alert_class = "alert-success"
+                      }
+                      document.getElementById('classification_guess').className = 'alert';
+                      document.getElementById('classification_guess').classList.add( alert_class );
+              }
+            });
+        }
+);
+});
